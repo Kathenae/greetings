8427aad730c46b99d40e64fd60fedda5f7426503
Ronald <ronaldmachado.work@gmail.com>
2026-01-14 06:57:04 +0200

    fix: return an error when given name is empty

--- greetings.go
+++ greetings.go
@@ -1,9 +1,15 @@
 package greetings
 
 import "fmt"
+import "errors"
 
 // Hello functions returns a greeting for the name of the person.
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+  // if no name was provided, return an error with a message
+  if name == "" {
+    return "", errors.New("empty name")
+  }
+
   // Return a greeting that embeds the name in a message.
   var message string
   message = fmt.Sprintf("Hi, %v. Welcome!", name)
